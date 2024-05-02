dfeea070630329f46243b9b78c40581e9f89beb2
onkhea <khea@xtricate.com>
2024-05-02 09:31:09 +0700

    api

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get("/data", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
